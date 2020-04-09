391d06cb0848081c9a3e34cf1ab756aae1bb49f8
Serge NOEL <serge.noel@net6a.com>
2020-04-09 14:16:09 +0200

    Menage de printemps

---
+++ www/_Old/webmaster/define.php
@@ -0,0 +1,9 @@
+<?php
+define("hote","AmapDevDb");   
+define("login","Amap");         /*   "root"  ou  "c.archidoit"   ou "amap.saintseb"           */
+define("mot_passe_sql","Secr3t");       /*   "" ou  "amaplesg"  ou "lesgumes" */
+define("base_de_donnees","Amap");
+
+define('JOURS_MARGE_CHEVRE',5);
+define('JOURS_MARGE_PDT_LAITIER',8);
+?>
